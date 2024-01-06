38fc1889347354d7384120311580743fe463deef
27Stanley <132085684+27Stanley@users.noreply.github.com>
2024-01-06 10:20:16 +0000

    Add files via upload

--- spotify_api/main.py
+++ spotify_api/main.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import os
 import base64
 import json
-from requests import post
+from requests import post, get
 
 
 load_dotenv()
@@ -28,9 +28,40 @@ def get_token():
     token = json_result["access_token"]
     return token
 
+
 def get_auth_header(token):
     return {"Authorization": "Bearer " + token}
 
+
+def search_for_artist(token, artist_name):
+    url = "https://api.spotify.com/v1/search"
+    headers = get_auth_header(token)
+    query = f"?q={artist_name}&type=artist&limit=1"
+
+    query_url = url + query
+    result = get(query_url, headers=headers)
+    json_result = json.loads(result.content)["artists"]["items"]
+    if len(json_result) == 0:
+        print("no artist with this name") 
+        return None
+    return(json_result[0])
+
+
+def get_songs_by_artist(token, artist_id):
+    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=HK"
+    headers = get_auth_header(token)
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)["tracks"]
+    return json_result
+
+
 token = get_token()
-print(token, "token here")
+# print(token, "token here")
+
+result = search_for_artist(token, "yoasobi")
+artist_id = result["id"]
+songs = get_songs_by_artist(token, artist_id)
+
+for idx, song in enumerate(songs):
+    print(f"{idx+1}.{song['name']}")
 
